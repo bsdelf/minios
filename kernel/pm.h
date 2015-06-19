e34827ca720fa0009497c159c001243eb8694c41
Yanhui Shen <shen.elf@gmail.com>
2015-06-19 18:44:18 +0800

    ~ minor change

--- kernel/pm.h
+++ kernel/pm.h
@@ -6,11 +6,10 @@
 #define PM_NORDER   11
 #define PM_NSEG     5
 
-void pm_init(void);
-void pm_add_seg(vm_addr_t start, vm_addr_t end, vm_page_t* pages, uint32 npage);
-vm_page_t* pm_alloc(uint8 order);
-void pm_release(vm_page_t* page, uint8 order);
-
-void pm_status();
+void        pm_init     (void);
+void        pm_status   (void);
+void        pm_add_seg  (vm_addr_t start, vm_addr_t end, vm_page_t* pages, uint32 npage);
+vm_page_t*  pm_alloc    (uint8 order);
+void        pm_release  (vm_page_t* page, uint8 order);
 
 #endif
